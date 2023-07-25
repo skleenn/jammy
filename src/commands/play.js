9f1e5d08b1f3e806ea29045fecd630720fc9da48
skleenn <113645968+skleenn@users.noreply.github.com>
2023-07-25 16:41:14 -0700

    spotify app

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,4 +1,18 @@
 const { SlashCommandBuilder } = require("discord.js");
+const { Manager } = require("erela.js");
+const Spotify = require("erela.js-spotify");
+
+const clientid = "334d81c7cafb482ca840de440de2a167";
+const clientsecret = "d010a39d6b54494ba4b34613bce702a8";
+
+const manager = new Manager({
+    plugins: [
+        new Spotify({
+            clientid,
+            clientsecret
+        })
+    ]
+});
 
 module.exports = {
     data: new SlashCommandBuilder()
