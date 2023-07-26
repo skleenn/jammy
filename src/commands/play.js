956ac338ea1ea64313b0f84b4ade6a4d700de076
skleenn <113645968+skleenn@users.noreply.github.com>
2023-07-25 17:47:34 -0700

    hfosiajijfaios;df

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, managerToFetchingStrategyOptions } = require("discord.js");
 const { Manager } = require("erela.js");
 const Spotify = require("erela.js-spotify");
 const { ErelaClient } = require("erela.js");
@@ -11,6 +11,16 @@ module.exports = {
         .addStringOption(option => option.setName("url").setDescription("enter spotify url of song!!")),
 
         async execute(client, interaction){
-            
+            const song = interaction.options.getString("song url");
+
+            if (!interaction.member.voice.channel) return interaction.reply({content:"you must be in a vc to use this command!!", ephemeral: true});
+
+            const player = client.music.players.spawn({
+                guild: message.guild,
+                voiceChannel: voiceChannel,
+                textChannel: message.channel,
+            });
+
+            const songs = await manager.search("url")
         }
 }
